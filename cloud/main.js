5429148ba548b1ee7beba1561dc50a41e883bc57
Heron Yang <heron.yang.tw@gmail.com>
2016-01-17 10:41:42 +0800

    test version 1

--- cloud/main.js
+++ cloud/main.js
@@ -1,6 +1,27 @@
 
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
-Parse.Cloud.define("hello", function(request, response) {
+Parse.Cloud.define("1/hello", function(request, response) {
   response.success("Hello world!");
+});
+
+Parse.Cloud.define("1/getPhotosOfGroup", function(request, response) {
+  var query = new Parse.Query("FlickrGroup");
+  query.equalTo("name", request.params.groupName);
+  query.find({
+      success: function(results) {
+        var r = results[0].relation("photos");
+        r.query().find({
+            success: function(photos){
+              response.success(photos); //list of trophies pointed to by that player's "trophies" column.
+            },
+            error: function(error){
+              response.error(error);
+            }
+        })
+      },
+      error: function() {
+        response.error("FlickrGroup lookup failed");
+      }
+  });
 });
